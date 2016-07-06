2f21c6bdaf5141d5a56346a23b364a73dadf7bb6
aa <aa@a.a>
2016-07-06 15:46:26 +0800

    手机京东

---
+++ js/fenlei.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#li3').click(function(){
+		$('.head-u2').slideToggle(500);
+	});
+	// for(var i=1;i<$('#left_ul li').length;i++){
+	// 	$('#l'+i).click(function(){
+	// 		var c=i-1;
+	// 		// $('#l'+c).hide();
+	// 		$(this).css('backgroundColor','#fff');
+			
+	// 	});
+	// }
+
+});
